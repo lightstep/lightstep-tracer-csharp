c23d0092767606dfd4ccbfc0ffd1e99a3e017335
Austin Parker <austin@lightstep.com>
2018-08-29 13:22:27 -0400

    address issues from review

---
+++ src/LightStep/Propagation/Keys.cs
@@ -0,0 +1,11 @@
+﻿namespace LightStep.Propagation
+{
+    public static class Keys
+    {
+        public const string BaggagePrefix = "ot-bg-";
+
+        public const string TraceId = "ot-traceid";
+        public const string SpanId = "ot-spanid";
+        public const string Sampled = "ot-sampled";
+    }
+}
