2d72e607fff195c224fd3fc13e4da73fa733511d
Austin Parker <austin@lightstep.com>
2018-08-31 12:55:40 -0400

    more docs

--- src/LightStep/Baggage.cs
+++ src/LightStep/Baggage.cs
@@ -3,32 +3,53 @@ using System.Collections.Generic;
 
 namespace LightStep
 {
+    /// <summary>
+    /// Baggage is a dictionary of key:value information that is propagated via the SpanContext.
+    /// </summary>
     public class Baggage
     {
         private readonly IDictionary<string, string> _items = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
+        /// <summary>
+        /// Sets key to value.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         public void Set(string key, string value)
         {
             _items[key] = value;
         }
 
+        /// <summary>
+        /// Gets value for key; Will return null if key does not exist.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public string Get(string key)
         {
             string value;
             return _items.TryGetValue(key, out value) ? value : null;
         }
 
+        /// <summary>
+        /// Gets all <see cref="KeyValuePair{TKey,TValue}"/> from baggage.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<KeyValuePair<string, string>> GetAll()
         {
             return _items;
         }
 
+        /// <summary>
+        /// Combines two baggage into one.
+        /// </summary>
+        /// <param name="other"></param>
         public void Merge(Baggage other)
         {
             Merge(other?.GetAll());
         }
 
-        public void Merge(IEnumerable<KeyValuePair<string, string>> other)
+        private void Merge(IEnumerable<KeyValuePair<string, string>> other)
         {
             if (other == null)
                 return;
--- src/LightStep/LightStepSpanRecorder.cs
+++ src/LightStep/LightStepSpanRecorder.cs
@@ -4,20 +4,24 @@ using OpenTracing;
 
 namespace LightStep
 {
+    /// <inheritdoc />
     public sealed class LightStepSpanRecorder : ISpanRecorder
     {
         private List<SpanData> Spans { get; }= new List<SpanData>();
-        
+
+        /// <inheritdoc />
         public void RecordSpan(SpanData span)
         {
             Spans.Add(span);
         }
-        
+
+        /// <inheritdoc />
         public IEnumerable<SpanData> GetSpanBuffer()
         {
             return Spans;
         }
 
+        /// <inheritdoc />
         public void ClearSpanBuffer()
         {
             Spans.Clear();
