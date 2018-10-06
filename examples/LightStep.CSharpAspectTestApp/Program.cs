59f7c15da7b967881369229dda99dbc0db3f8b88
Austin Parker <austinlparker@gmail.com>
2018-10-06 13:51:42 -0400

    remove ls key

--- examples/LightStep.CSharpAspectTestApp/Program.cs
+++ examples/LightStep.CSharpAspectTestApp/Program.cs
@@ -1,5 +1,7 @@
+using LightStep.CSharpAspectTestApp.Aspects;
 using OpenTracing.Util;
 using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +13,7 @@ namespace LightStep.CSharpAspectTestApp
     {
         static void Main(string[] args)
         {
-            var lsKey = "7462e3fc8a93e171b1524cec623700f5";
+            var lsKey = ConfigurationManager.AppSettings["lsKey"];
             var lsSettings = new SatelliteOptions("collector.lightstep.com");
             var lsOptions = new Options(lsKey, lsSettings);
             lsOptions.UseHttp2 = false;
@@ -22,20 +24,21 @@ namespace LightStep.CSharpAspectTestApp
 
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            using (var scope = tracer.BuildSpan("main").StartActive())
-            {
-                Console.WriteLine(scope.Span.Context.SpanId);
-                var client = new HttpWorker();
-                client.Get("https://jsonplaceholder.typicode.com/todos/1");
-            }
 
+            DoThing();
+            
             //tracer.Flush();
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
 
+        [Traceable]
+        static void DoThing()
+        {
+            var client = new HttpWorker();
+            client.Get("https://jsonplaceholder.typicode.com/todos/1");
+        }
 
     }
 }
