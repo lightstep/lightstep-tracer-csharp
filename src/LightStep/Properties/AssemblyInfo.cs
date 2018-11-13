9a1b153cd3bd3b8c0f573028d12d92147dec36b1
Austin Parker <austin@lightstep.com>
2018-11-13 10:17:56 -0500

    add cake and versioning (#32)

---
+++ src/LightStep/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("0.1.29.0")]
+[assembly: AssemblyVersion("0.1.29.0")]
+[assembly: AssemblyFileVersion("0.1.29.0")]
+[assembly: AssemblyInformationalVersion("0.1.29-enhancement-addCake.1-2018-11-12")]
