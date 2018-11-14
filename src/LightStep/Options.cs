470106c25667c5a88827863c469309d5281bd054
Austin Parker <austin@lightstep.com>
2018-11-14 11:41:50 -0500

    inspect target framework attribute if appdomain doesn't have info

--- src/LightStep/Options.cs
+++ src/LightStep/Options.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Versioning;
 
@@ -128,7 +129,18 @@ namespace LightStep
         private static string GetPlatformVersion()
         {
 #if NET45
-            return AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+            var version = "";
+            version = AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+            if (version == null)
+            {
+                TargetFrameworkAttribute tfa = (TargetFrameworkAttribute) Assembly.GetEntryAssembly().GetCustomAttributes(typeof(TargetFrameworkAttribute))
+                    .SingleOrDefault();
+                if (tfa != null)
+                {
+                    version = tfa.FrameworkName;    
+                }
+            }
+            return version;
 #elif NETSTANDARD2_0
             return Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
 #else
