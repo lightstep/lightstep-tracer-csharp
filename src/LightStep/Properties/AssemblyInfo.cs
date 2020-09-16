f174fbbebfba4e93a6636cecf8a5d93050ab3f56
Austin Parker <austin@lightstep.com>
2020-09-16 08:10:17 -0400

    fix some nits

--- src/LightStep/Properties/AssemblyInfo.cs
+++ src/LightStep/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0.0")]
-[assembly: AssemblyCopyright("Copyright (c) LightStep 2018 - 2019")]
+[assembly: AssemblyCopyright("Copyright (c) LightStep 2018 - 2020")]
 
 [assembly: InternalsVisibleTo("LightStep.Tests,PublicKey=002400000480000094000000060200000024000052534131000400000100010099deeadb052e9763d2dc7827700d80e349e5d16585c92416171e6689a4bd38a3acea971d5899d5e2cd4239c3dc799558138e961f8d0f5095fef969672172833868f2cc2d908970370af834beef9dad328182fee2aaf0d0bb568ffd1f829362b88718734541d334c6a2cdf0049f5a0ee5e4962d0db3f49f86bf742f9531bd9c8c")]
 
